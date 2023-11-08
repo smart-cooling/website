8ac86f7566143e8f93206aae6324249f791b07fe
mvacoimbra <mv27.marcosvinicius@gmail.com>
2023-11-08 12:05:52 -0300

    fix: hero section background color

--- app/components/sections/Hero.tsx
+++ app/components/sections/Hero.tsx
@@ -3,13 +3,14 @@ import { Transition } from "../Transition";
 
 export default function Hero() {
   const bg =
-    "bg-[url('https://res.cloudinary.com/mvacoimbra/image/upload/v1699317522/smart-cooling-website/hero-bg.png')]";
+    "bg-[url('https://res.cloudinary.com/mvacoimbra/image/upload/v1699455524/smart-cooling-website/hero-bg.png')]";
 
   return (
     <section
-      className={`${bg} relative flex h-screen items-center bg-cover bg-center bg-no-repeat px-7 text-w-1 md:px-24`}
+      className={`relative flex h-screen items-center px-7 text-w-1 md:px-24 ${bg} bg-cover bg-center bg-no-repeat`}
     >
-      <div>
+      <div className="absolute left-0 top-0 z-0 h-screen w-screen bg-gradient-to-r from-o-1 via-o-4 to-b-1 opacity-90 mix-blend-multiply" />
+      <div className="z-10">
         <h1 className="mb-1 text-4xl font-medium md:text-6xl">
           {"Seu conforto é a nossa missão!"}
         </h1>
@@ -18,6 +19,7 @@ export default function Hero() {
             "Descubra o conforto climático e a eficiência energética que você merece. Sua jornada começa aqui."
           }
         </h6>
+
         <Transition
           className="absolute bottom-0 left-0 w-screen"
           asset="hero-transition"
